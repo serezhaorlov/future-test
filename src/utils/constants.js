ed797582a8bf7b2be778555c4780af693584408f
Serezha Orlov <somepuppeys@yandex.ru>
2021-02-19 23:50:50 +0300

    fixed https for gh-pages

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,5 +3,7 @@ export const smallUsersBase = 'http://www.filltext.com/?rows=32&id=%7Bnumber%7C1
 export const bigUsersBase = 'http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D'
 
 
+const letterA ='a'
+
 
 
